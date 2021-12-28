eb122d028afeecac86af01dbdca9bbf3847931b5
abhishekupadhyaya477 <72117184+abhishekupadhyaya477@users.noreply.github.com>
2021-12-28 22:49:05 +0530

    First commit

---
+++ javaScript/index.js
@@ -0,0 +1,175 @@
+let inputDir={ x:0, y:0};
+const foodSound= new Audio('eat.mp3');
+const moveSound= new Audio('move1.wav');
+const coinSound=new Audio('coin.wav')
+const gameOverSound=new Audio('gameOver.mp3');
+let speed=10;
+let score=0;
+let highScoreVal=0;
+let lastPaintTime=0;
+let snakeArr=[
+{ x:13 , y:15}
+]
+let food=  {x:3, y:4};
+let food1= {x:4 , y:3}
+
+// GAME FUNCTIONS
+function main(ctime) {
+    window.requestAnimationFrame(main)
+    // console.log(ctime)
+
+    if((ctime-lastPaintTime)/1000<1/speed)
+    {
+        return;
+    }
+    lastPaintTime=ctime;
+    gameEngine();
+}
+function isCollide(snake) {
+    // If you bump into yourself
+    for (let i= 1; i < snake.length; i++) {
+        if(snake[i].x===snake[0].x&&snake[i].y===snake[0].y)
+        {
+            return true;
+        }
+    }
+    // If you bumb in the wall
+    if(snake[0].x>18||snake[0].x<1||snake[0].y>18||snake[0].y<1)
+    {
+        return true;
+    } 
+
+    
+}
+function gameEngine() {
+    // Part 1: Updating the snake array and food
+    if(isCollide(snakeArr)){
+        gameOverSound.play();
+        inputDir={x:0,y:0}
+        alert("Game over! Press any key to play again");
+        snakeArr=[{x:13, y:15}];
+        score=0;
+        scoreBox.innerHTML="Score="+score;
+    }
+
+    // If you have eaten the food, increament the score and regenerate the food
+    if(snakeArr[0].y==food.y&&snakeArr[0].x==food.x||snakeArr[0].y==food1.y&&snakeArr[0].x==food1.x){
+        if(snakeArr[0].y==food.y&&snakeArr[0].x==food.x)
+        {
+            foodSound.play();
+            score+=1;
+        }
+        if(snakeArr[0].y==food1.y&&snakeArr[0].x==food1.x)
+        {
+            coinSound.play();
+            score+=2;
+        }
+
+        
+        if (score>highScoreVal) {
+            highScoreVal=score;
+            localStorage.setItem("highScore",JSON.stringify(highScoreVal));
+            HighScoreBox.innerHTML="High Score = "+highScoreVal;
+        }
+        scoreBox.innerHTML="Score="+score;
+        snakeArr.unshift({x:snakeArr[0].x+inputDir.x ,y:snakeArr[0].y+inputDir.y})
+        let a=1;
+        let b=16;
+        if(score%10==0)
+        {
+            food1 ={x:Math.round(a+(b-a)*Math.random()) ,y:Math.round(a+(b-a)*Math.random())}
+        }
+        else{
+        food ={x:Math.round(a+(b-a)*Math.random()) ,y:Math.round(a+(b-a)*Math.random())}
+        }
+    }
+
+
+
+
+    // Moving the snake
+    for (let i = snakeArr.length-2; i >=0; i--) {
+        snakeArr[i+1]={...snakeArr[i]}; //for refference problem
+        
+    }
+    snakeArr[0].x+=inputDir.x;
+    snakeArr[0].y+=inputDir.y;
+
+
+    // Part 2: Display the snake array and food
+
+    // Display the snake
+    board.innerHTML="";
+    snakeArr.forEach((e , index)=>{
+        snakeElement=document.createElement('div');
+        snakeElement.style.gridRowStart=e.y;
+        snakeElement.style.gridColumnStart=e.x;
+        if(index==0){
+            snakeElement.classList.add('head');
+        }
+        else{
+            snakeElement.classList.add('snake');
+        }
+        board.appendChild(snakeElement);
+    });
+
+    // Display the food
+    if(score!=0&&score%5==0)
+    {
+    foodElement=document.createElement('div');
+    foodElement.style.gridRowStart=food1.y;
+    foodElement.style.gridColumnStart=food1.x;
+    foodElement.classList.add('food1');
+    board.appendChild(foodElement);
+    }
+    foodElement=document.createElement('div');
+    foodElement.style.gridRowStart=food.y;
+    foodElement.style.gridColumnStart=food.x;
+    foodElement.classList.add('food');
+    board.appendChild(foodElement);
+    
+
+}
+
+
+
+
+// MAIN LOGIC
+let highScore=localStorage.getItem("highScore");
+if(highScore===null){
+    highScoreVal=0;
+    localStorage.setItem("highScore",JSON.stringify(highScoreVal))
+}
+else{
+    HighScoreBox.innerHTML="High Score = "+highScore;
+}
+
+window.requestAnimationFrame(main)
+window.addEventListener('keydown' ,e=>{
+    inputDir={x:0, y:1}
+    moveSound.play();
+    switch (e.key) {
+        case "ArrowUp":
+            inputDir.x=0;
+            inputDir.y=-1;
+            break;
+        case "ArrowDown":
+            inputDir.x=0;
+            inputDir.y=1;
+            break;
+        case "ArrowLeft":
+            inputDir.x=-1;
+            inputDir.y=0;
+            break;
+        case "ArrowRight":
+            inputDir.x=1;
+            inputDir.y=0
+            break;
+    
+        default:
+            //  inputDir.x=0;
+            //  inputDir.y=0;
+            break;
+    }
+
+})
